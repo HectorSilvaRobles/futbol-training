83485abe8fa8e437267b36e10e806c5367cc2be4
HSR <hectorsilvarobles@gmail.com>
2020-01-24 14:07:50 -0700

    adding firebase function

---
+++ Firebase/functions/index.js
@@ -0,0 +1,48 @@
+const functions = require('firebase-functions');
+const {Storage} = require('@google-cloud/storage');
+const projectId = 'futbol-training'
+const gcs = new Storage({
+    projectId
+})
+const os = require('os')
+const path = require('path')
+const spawn = require('child-process-promise').spawn
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+
+
+exports.whenFileChange = functions.storage.object().onFinalize((event) => {
+    const bucket = event.bucket;
+    const contentType = event.contentType;
+    const filePath = event.name;
+
+    console.log('File change detected, function exectuion started');
+    console.log('this is the event', event)
+
+    if(event.resourceState === 'not_exists' ){
+        console.log('We deleted the file')
+        return;
+    }
+
+    if(path.basename(filePath).startsWith('resized-')){
+        console.log('We already renamed that file!')
+        return;
+    }
+
+    // Temporary storage for file
+    const destBucket = gcs.bucket(bucket);
+    const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath))
+    const metadata = {contentType: contentType}
+
+    return destBucket.file(filePath).download({
+        destination: tmpFilePath
+    }).then(() => {
+        return spawn('convert', [tmpFilePath, '-resize', '500x500', tmpFilePath]);
+    }).then(() => {
+        return destBucket.upload(tmpFilePath, {
+            destination: 'resized-' + path.basename(filePath),
+            metadata: metadata
+        })
+    });
+});
