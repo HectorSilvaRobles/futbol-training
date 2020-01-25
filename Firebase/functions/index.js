5dc4bc8ef36500d63efcb83a03d436eefc9a547c
HSR <hectorsilvarobles@gmail.com>
2020-01-24 23:09:16 -0700

    making http endpoint in firebase

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,12 +1,17 @@
 const functions = require('firebase-functions');
 const {Storage} = require('@google-cloud/storage');
 const projectId = 'futbol-training'
+const keyFileName = 'futbol-training-firebase-adminsdk-ubaps-5c11a1da2f.json'
 const gcs = new Storage({
-    projectId
+    projectId,
+    keyFileName
 })
 const os = require('os')
 const path = require('path')
 const spawn = require('child-process-promise').spawn
+const cors = require('cors')({origin: true});
+const Busboy = require('busboy');
+const fs = require('fs')
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -44,5 +49,47 @@ exports.whenFileChange = functions.storage.object().onFinalize((event) => {
             destination: 'resized-' + path.basename(filePath),
             metadata: metadata
         })
+    });
+});
+
+exports.uploadFile = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        if(req.method !== 'POST'){
+            return res.status(500).json({
+                message: 'Not allowed, only POST requests allowed'
+            })
+        }
+
+        const busboy = new Busboy({headers: req.headers})
+        let uploadData = null;
+
+        busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+            const filepath = path.join(os.tmpdir(), filename);
+            uploadData = {file: filepath, type: mimetype}
+            file.pipe(fs.createWriteStream(filepath));
+        });
+
+        busboy.on('finish', () => {
+            const bucket = gcs.bucket('futbol-training.appspot.com')
+            bucket.upload(uploadData.file, {
+                uploadType: 'media',
+                metadata: {
+                    metadata: {
+                        contentType: uploadData.type
+                    }
+                }
+            })
+            .then(() => {
+                return res.status(200).json({
+                    message: 'It worked ~_~'
+                });
+            })
+            .catch(err => {
+                return res.status(500).json({
+                    error: err
+                });
+            });
+        });
+        busboy.end(req.rawBody)
     });
 });
