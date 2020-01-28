1bd9f0eaf9b0ef9c1980eb24f1bd72f00736f2e9
HSR <hectorsilvarobles@gmail.com>
2020-01-28 10:47:52 -0700

    setting up athlete endpoints

--- server/index.js
+++ server/index.js
@@ -27,6 +27,9 @@ mongoose.connect(`${process.env.MONGODB_URI}`, {
 // Coach User endpoints
 app.use('/api/coach-users', require('./apiRoutes/coach_user_route'));
 
+app.use('/api/athletes', require('./apiRoutes/athlete_route'));
+
+
 const port = process.env.PORT || 4000
 
 app.listen(port, () => console.log(`server running on port ${port}`))
