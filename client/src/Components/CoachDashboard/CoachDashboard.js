e25e0df5d57b2de5ec29be52e93dee5479dc94e9
HSR <hectorsilvarobles@gmail.com>
2020-02-04 10:43:49 -0700

    show components if user is admin

--- client/src/Components/CoachDashboard/CoachDashboard.js
+++ client/src/Components/CoachDashboard/CoachDashboard.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import ManageAthletes from './ManageAthletes/ManageAthletes'
+import {useSelector} from 'react-redux'
+;
+function CoachDashboard (props){
+    // Check if coach user is 'Admin' in account role
+    const coach_user = useSelector(state => state.coach_user)
+    const {userData} = coach_user;
+    let show = false
 
-export class CoachDashboard extends Component {
-    render() {
-        return (
-            <div>
-                admin coach
-                <ManageAthletes />
-            </div>
-        )
+    if(userData){
+        const {accountRole} = userData
+        if(accountRole == "Admin"){
+            show = true
+        }
     }
+    
+
+    return (
+        <div>
+            admin coach
+            {show ? <ManageAthletes /> : null}
+            {/* <ManageAthletes /> */}
+         </div>
+    )
 }
+
 
 export default CoachDashboard
