6f60fe6609a0e0e3dcea10f37f04b84c4d914050
HSR <hectorsilvarobles@gmail.com>
2020-02-08 18:22:08 -0700

    got embedded data to work, can now create coach post and can also remove post

---
+++ server/apiRoutes/coach_to_athlete_route.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const {Athletes} = require('../models/athleteSchema')
+
+// Create a new coach post 
+router.put('/coach-post/:athlete_id', (req, res, next) => {
+    const param = req.params.athlete_id
+    Athletes.findByIdAndUpdate(param, { $push: req.body }, (err, updated_athlete) => {
+        if(err){
+            return res.status(400).json({
+                message: 'Could not create new coach post to athlete newsfeed',
+                error: err
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Successfully created a new coach post to athlete's newsfeed",
+            updated_athlete: updated_athlete
+        })
+    })
+})
+
+
+// Removing coach post
+router.delete('/remove-coach-post/:athlete_id', (req, res) => {
+    const param = req.params.athlete_id
+    const post = req.body.post_id
+
+    Athletes.updateOne(
+        {_id: param}, 
+        {$pull : {'coach_posts' : {"_id" : post}}}
+    ).then(() => res.status(200).json({
+        success: true,
+        message: "Successfully removed a coach post from athlete's newsfeed",
+    }))
+    .catch(err => res.status(400).json({
+        success: false,
+        message: "Could not remove coach post from athlete's newsfeed",
+        error: err
+    }))
+})
+
+
+module.exports = router
