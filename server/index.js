6ad589c0ebdc79e43247bbbd800c1b5900d584cc
HSR <hectorsilvarobles@gmail.com>
2020-04-01 16:35:31 -0700

    oh

--- server/index.js
+++ server/index.js
@@ -35,11 +35,14 @@ app.use('/api/coach_to_athlete', require('./apiRoutes/coach_to_athlete_route'))
 // get all pending requests
 app.use('/api/pending', require('./apiRoutes/pending_route'))
 
-const port = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 }
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
 
-app.listen(port, () => console.log(`server running on port ${port}`))
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
