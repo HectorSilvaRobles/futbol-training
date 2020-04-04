90619ee305ae7387ff21b82152e941ebf9b81e23
HSR <hectorsilvarobles@gmail.com>
2020-04-03 22:28:11 -0700

    h

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use('/api/pending', require('./apiRoutes/pending_route'))
 const PORT = 4000
 
 if(process.env.NODE_ENV == 'production'){
-    app.use(express.static('client/build'))
+    // app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
