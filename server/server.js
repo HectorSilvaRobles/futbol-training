dd4ad771d1b68495e21994c2bfb2967c3ce2a188
HSR <hectorsilvarobles@gmail.com>
2020-04-11 18:43:48 -0700

    stolen

--- server/server.js
+++ server/server.js
@@ -38,10 +38,10 @@ app.use('/api/pending', require('./apiRoutes/pending_route'))
 const PORT = 4000
 
 if(process.env.NODE_ENV == 'production'){
-    app.use(express.static(path.join("client/build")));
+    app.use(express.static(path.join("/client/build")));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname +  'client/build/index.html'))
+        res.sendFile(path.resolve(__dirname +  '/client/build/index.html'))
     })
 }
 
